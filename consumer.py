3d7146d277fa8253a0daabc6fdec613aaafbd822
WolfGunMan <92699163+WolfGunMan@users.noreply.github.com>
2024-01-24 08:37:48 -0300

    Version 2
    - Se modifico el codigo para mayor facilidad en caso de necesitar agregar nuevas colecciones para consumir

--- consumer.py
+++ consumer.py
@@ -2,61 +2,70 @@ import json
 from confluent_kafka import Consumer, KafkaError
 from pymongo import MongoClient
 
-# Iinitialize consumer kafka
-consumer_conf = {
-    'bootstrap.servers': 'localhost:9092',
-    'group.id': 'thisgroup',
-    'auto.offset.reset': 'earliest'
-}
+def initialize_mongo_connection(collection_name):
+    mongo_client = MongoClient('mongodb+srv://root:root@cluster.esryp20.mongodb.net/?retryWrites=true&w=majority')
+    mongo_db = mongo_client['mongodb']
+    mongo_collection = mongo_db[collection_name]
+    return mongo_collection
 
-consumer = Consumer(consumer_conf)
-
-# Initialize mongodb connection
-mongo_client = MongoClient('mongodb+srv://root:root@cluster.esryp20.mongodb.net/?retryWrites=true&w=majority')
-mongo_db = mongo_client['mongodb']
-mongo_collection = mongo_db['users']
-
-# Subscribe to kafka topic
-consumer.subscribe(['dbserver1.public.users'])
-
-print("Start consumer mongo collection")
-
-# Función para procesar eventos y actualizar MongoDB
 def process_event(event_data, mongo_collection):
     print("new message: :)")
-    # Implementa la lógica de procesamiento aquí
-
-    # Insert a new user into the collection
-    if event_data['payload']['op'] == 'c':  # 'c' indica una operación de inserción
+    if event_data['payload']['op'] == 'c':
         document = event_data['payload']['after']
         mongo_collection.insert_one(document)
-
-    # Update User in MongoDB
     elif event_data['payload']['op'] == 'u':
         document_id = event_data['payload']['after']['id']
         update_data = event_data['payload']['after']
         mongo_collection.update_one({'id': document_id}, {'$set': update_data})
 
+def main():
+    # Configuración de Kafka
+    consumer_conf = {
+        'bootstrap.servers': 'localhost:9092',
+        'group.id': 'thisgroup',
+        'auto.offset.reset': 'earliest'
+    }
 
-# Process events
-try:
-    while True:
-        msg = consumer.poll(1.0)
+    # Lista de colecciones y sus respectivos temas de Kafka
+    collections = {
+        'users': 'dbserver1.public.users',
+        #'autos': 'dbserver1.public.autos'
+        # Agrega más colecciones según sea necesario
+    }
 
-        if msg is None:
-            continue
-        if msg.error():
-            if msg.error().code() == KafkaError._PARTITION_EOF:
-                continue
-            else:
-                print(msg.error())
-                break
+    # Inicializa consumidores de Kafka y conexiones de MongoDB
+    consumers = {}
+    mongo_collections = {}
+    for collection_name, kafka_topic in collections.items():
+        consumer = Consumer(consumer_conf)
+        consumer.subscribe([kafka_topic])
+        consumers[collection_name] = consumer
+        mongo_collections[collection_name] = initialize_mongo_connection(collection_name)
 
-        # Proces message
-        event_data = json.loads(msg.value())
-        process_event(event_data, mongo_collection)
+    print("Start consumer mongo collections")
 
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
+    try:
+        while True:
+            for collection_name, consumer in consumers.items():
+                msg = consumer.poll(1.0)
+
+                if msg is None:
+                    continue
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        continue
+                    else:
+                        print(msg.error())
+                        break
+
+                event_data = json.loads(msg.value())
+                process_event(event_data, mongo_collections[collection_name])
+
+    except KeyboardInterrupt:
+        pass
+    finally:
+        for consumer in consumers.values():
+            consumer.close()
+
+if __name__ == "__main__":
+    main()
